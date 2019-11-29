b8bcad08e92672345481fbdaf50486855ab7641a
michael <mikelkarije@gmail.com>
2019-11-29 19:32:17 +0300

    initialized bootstarp and also imported it so that it makes the app work with styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask 
 from config import Config
-
+from flask_bootstrap import Bootstrap
 
 def create_app(config_name):
     
     app = Flask(__name__)
-
+    bootstrap = Bootstrap(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
