900ef67f597c7cf0af03acfbdef778b255414973
michael <mikelkarije@gmail.com>
2019-12-01 13:48:05 +0300

    configuration of database linkusing sqlalchemy link

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 class Config:
- pass
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://michael:joker@localhost/mickeypost'
+    SQLALCHEMY_TRACK_MODIFICATIONS = 'True'
 
 class ProConfig(Config):
   pass
     
 class DevConfig(Config):
     DEBUG = True
+    
+# config_options = {
+#     'production' : ProdConfig,
+#     'development': DevConfig
+# }
