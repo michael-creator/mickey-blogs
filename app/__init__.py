59eb9ac59b68b90b9832efbf5a0557219c938441
michael <mikelkarije@gmail.com>
2019-12-01 13:45:31 +0300

    imported sqlalchemy into the init file to initialize database into my work

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask 
 from config import Config
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 def create_app(config_name):
     
     app = Flask(__name__)
-    bootstrap = Bootstrap(app)
+    bootstrap.init_app(app)
+    db.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
