66ac86bc5e7488f102b059a7019b354863755f9a
michael <mikelkarije@gmail.com>
2019-12-01 22:54:14 +0300

    importation of both forms and login manager to authenticate the user

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -10,6 +15,7 @@ def create_app(config_name):
     app = Flask(__name__)
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
