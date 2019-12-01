1e23f9cb151645545680254407f54a313cc7e2dd
michael <mikelkarije@gmail.com>
2019-12-01 20:50:30 +0300

    addition of new route called the about html template

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 from.import main
 
 
-def index():
-    return render_template(index.html)
+
 @main.route('/')
 def index():
     
     return render_template('index.html')
+
+@main.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
