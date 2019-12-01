6b2c3b622420e0919f153d24ffcfd3f88580f460
michael <mikelkarije@gmail.com>
2019-12-01 22:56:04 +0300

    added a new comment route tonavigate to the blog posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from.import main
+from flask_login import login_required
 
 
 
@@ -8,6 +9,18 @@ def index():
     
     return render_template('index.html')
 
-@main.route('/about', methods=['GET'])
+@main.route('/about',methods = ['GET','POST'])
 def about():
     return render_template('about.html')
+
+
+@main.route('/contact',methods = ['GET','POST'])
+def contact():
+        
+    return render_template('contact.html')
+
+
+@main.route('/new_review',methods = ['GET','POST'])
+def new_review():
+ 
+    return render_template('new_review.html')
