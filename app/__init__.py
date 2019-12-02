d49afd08612b30f3e4a2c1585219e1e19033af65
michael <mikelkarije@gmail.com>
2019-12-02 18:31:39 +0300

    importation of config file in our app and created its instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-from config import Config
+from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -13,6 +13,8 @@ db = SQLAlchemy()
 def create_app(config_name):
     
     app = Flask(__name__)
+    
+    app.config.from_object(config_options[config_name])
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
