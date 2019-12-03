38dd6a0a942f5d920d9419f607a5aa9985d3aa80
michael <mikelkarije@gmail.com>
2019-12-03 10:10:48 +0300

    quote link url was added to class config to be defined

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config:
 
   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michael:mickey22@localhost/mickeypost'
-
+  QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
   SECRET_KEY = os.environ.get('SECRET_KEY')
   # SQLALCHEMY_TRACK_MODIFICATION = True
 
