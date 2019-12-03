0f1b9add24e262204181534813eb49ef0a58bd65
michael <mikelkarije@gmail.com>
2019-12-03 10:11:14 +0300

    addition of migration and datavase commands

--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from app.models import User
-
+from  flask_migrate import Migrate, MigrateCommand
 
 app=create_app('development')
 manager = Manager(app)
 manager.add_command('Server',Server)
-
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User )
