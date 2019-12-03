094bbe3db0bd415e7bc226e218c28d300ffc37ed
michael <mikelkarije@gmail.com>
2019-12-03 10:01:18 +0300

    qoutes and its function for requesting a response to get quotes from Quote API

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User
 from.import main
 from flask_login import login_required
-
-
+from app.request import GetQuotes
+from .forms import UpdateProfile
+from .. import db
 
 @main.route('/')
 def index():
-    
-    return render_template('index.html')
+    quotes = GetQuotes()
+    print(quotes)
+    return render_template('index.html',quotes=quotes)
 
 @main.route('/about',methods = ['GET','POST'])
 def about():
@@ -28,3 +31,31 @@ def new_review():
 @main.route('/blog')
 def blog():
     return render_template('blog.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
