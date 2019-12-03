b8e13bfcfe15929aeed6889439ddbe9e2cb8105a
michael <mikelkarije@gmail.com>
2019-12-03 10:08:57 +0300

    added validation for login form and its new commands

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from . import db
 from flask_login import UserMixin
 from . import login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -8,13 +13,27 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
-    password_hash = db.Column(db.String(255))
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+    password_secure = db.Column(db.String(255))
+    bio= db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
 
+    def verify_password(self,password):
+        return check_password_hash(self.password_secure,password)
+
+class Quotes:
+    def __init__(self,author,quote):
+        self.author = author
+        self.quote = quote
+    
 
 
-    def __repr__(self):
-        return f'User {self.username}'
+    
